ee95590642bca31c8ee0f173e1af8677e323e98c
Krizthian <cristianortizmorales2013@gmail.com>
2018-01-07 07:27:20 -0500

    jfltexx: Dot-ify

---
+++ dot.mk
@@ -0,0 +1,17 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from jfltexx device
+$(call inherit-product, device/samsung/jfltexx/device.mk)
+
+$(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
+
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := full_jfltexx
+PRODUCT_DEVICE := jfltexx
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-I9505
