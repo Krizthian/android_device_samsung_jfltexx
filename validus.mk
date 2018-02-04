164e47105f2f520d2475efda23c514607f2b48b5
Krizthian <cristianortizmorales2013@gmail.com>
2018-02-03 19:09:31 -0500

    jfltexx: Validus

--- validus.mk
+++ validus.mk
@@ -6,11 +6,11 @@ $(call inherit-product, device/samsung/jfltexx/device.mk)
 
 $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 
-# Inherit some common DotOS stuff.
-$(call inherit-product, vendor/dot/config/common_full_phone.mk)
+# Inherit some common Validus stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := dot_jfltexx
+PRODUCT_NAME := validus_jfltexx
 PRODUCT_DEVICE := jfltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
